3240e1f0e1a2e46ff1b405b9a968f59beb533f59
mariefilabawag <mariefilabawag@gmail.com>
2025-05-23 21:19:50 +0800

    revision for index.js

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-app.use(cors());
+const corsOptions = {
+    origin: [
+        "http://localhost:3000",
+        "http://localhost:8000",
+        "https://fitnessapi-abawag-1.onrender.com"
+    ],
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 4000;
 
 mongoose.connect(process.env.MONGODB_URI);
+
 mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atlas.'));
 
 app.get('/', (req, res) => {
@@ -20,4 +32,7 @@ const userRoutes = require('./routes/user');
 const workoutRoutes = require('./routes/workout');
 app.use(userRoutes);
 app.use(workoutRoutes);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = { app, mongoose };
